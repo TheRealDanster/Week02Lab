fe80e274b839f756b63e24cc2b8998b9dc594f3a
cprg352 <cprg352@demongeek.com>
2020-09-15 12:46:29 -0600

    Week 2 Lab completed

---
+++ src/week2lab/Week2Lab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2lab;
+
+/**
+ *
+ * @author cprg352
+ */
+public class Week2Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is my week 2 lab submission. Hi!");
+    }
+    
+}
